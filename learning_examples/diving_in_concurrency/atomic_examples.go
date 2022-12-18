3b0acd4e4f3c607726cfcb139225abafb2960269
Кияница Денис <kiyanitsadenis@gmail.com>
2022-12-18 16:00:13 +0700

    add atomic example

---
+++ learning_examples/diving_in_concurrency/atomic_examples.go
@@ -0,0 +1,27 @@
+package diving_in_concurrency
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	//first example with addint32
+	value1 := int32(0)
+	atomic.AddInt32(&value1, 1)
+	fmt.Println(value1)
+
+	//second example with atomic interface
+	value2 := atomic.Value{}
+	value2.Store(100)
+	ct := value2.Load()
+	fmt.Println(ct)
+
+	//third example with context: lose performance
+	ctx := context.Background()
+	value3 := atomic.Value{}
+	value3.Store(&ctx)
+	ctFromThird := value3.Load().(context.Context)
+	fmt.Println(ctFromThird)
+}
